08bbed0ac80745bafbb11a2e06bd596ccbaddab5
shayan217 <mhasan0316@gmail.com>
2025-05-14 21:56:21 +0500

    3rd changes

--- lib/View/Login_SignUp_Screens/login_mode.dart
+++ lib/View/Login_SignUp_Screens/login_mode.dart
@@ -10,107 +10,105 @@ class LoginMode extends StatefulWidget {
   @override
   State<LoginMode> createState() => _LoginModeState();
 }
+
 class _LoginModeState extends State<LoginMode> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+        backgroundColor: Colors.white,
         body: Center(
             child: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(
-            height: 70,
-          ),
-
-          ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset("assets/images/playstore.png",scale: 3,)),
-          const SizedBox(
-            height: 150,
-          ),
-
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color:   ColorConstant().appColor,// Set border radius here
-            ),
-            width: 250,
-            height: 50,
-            child: TextButton(
-                onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => ContinueUsingPhone()),
-                  // );
-                  // Get.to(EmailAuthTabScreen());
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 70,
+              ),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    "assets/images/logo2.jpeg",
+                    scale: 3,
+                  )),
+              const SizedBox(
+                height: 150,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: ColorConstant().appColor, // Set border radius here
+                ),
+                width: 250,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => ContinueUsingPhone()),
+                      // );
+                      // Get.to(EmailAuthTabScreen());
                       Get.to(ContinueUsingEmail());
-
-                },
-                child: const Text(
-                  'CONTINUE USING PHONE',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 1,
-                      wordSpacing: 0.1,
-                      height: 1,
-                      fontFamily: 'Kanit'),
-                )),
+                    },
+                    child: const Text(
+                      'CONTINUE USING PHONE',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 1,
+                          wordSpacing: 0.1,
+                          height: 1,
+                          fontFamily: 'Kanit'),
+                    )),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: ColorConstant().appColor,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                width: 250,
+                height: 50,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ContinueUsingEmail()),
+                      );
+                    },
+                    child: const Text(
+                      'CONTINUE USING EMAIL',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 1,
+                          wordSpacing: 0.1,
+                          height: 1,
+                          fontFamily: 'Kanit'),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Spacer(),
+              const Text(
+                'Version 1.0',
+                style: TextStyle(
+                    fontSize: 19, color: Colors.black, fontFamily: 'Kanit'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 15,
-          ),
-
-          Container(
-            decoration: BoxDecoration(
-              color:  ColorConstant().appColor,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            width: 250,
-            height: 50,
-            child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ContinueUsingEmail()),
-                  );
-                },
-                child: const Text(
-                  'CONTINUE USING EMAIL',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 1,
-                      wordSpacing: 0.1,
-                      height: 1,
-                      fontFamily: 'Kanit'),
-                )),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-          const Spacer(),
-          const Text(
-            'Version 1.0',
-            style: TextStyle(
-                fontSize: 19, color: Colors.black, fontFamily: 'Kanit'),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-
-        ],
-      ),
-    )));
+        )));
   }
 }
--- lib/View/SplashScreen.dart
+++ lib/View/SplashScreen.dart
@@ -1,4 +1,54 @@
+// import 'package:betapp/View/BPro_App_Wallet/bpro_app_wallet_main.dart';
+// import 'package:betapp/View/onboarding/onbording.dart';
+// import 'package:betapp/utills/constants.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     firebaseAuth.authStateChanges().listen((User? user) {
+//       if (user == null) {
+//         Future.delayed(const Duration(seconds: 2)).then((value) =>
+//             Navigator.push(
+//                 context, MaterialPageRoute(builder: (_) => Onbording())));
+//       } else {
+//         Future.delayed(const Duration(seconds: 2)).then((value) =>
+//             Navigator.pushAndRemoveUntil(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => BProWalletMain()),
+//                 (route) => false));
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Center(
+//         child: ClipRRect(
+//             borderRadius: BorderRadius.circular(12),
+//             child: Image.asset(
+//               // "assets/images/playstore.png",
+//               "assets/images/logo2.jpeg",
+//               scale: 3,
+//             )),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:betapp/View/BPro_App_Wallet/bpro_app_wallet_main.dart';
+import 'package:betapp/View/Login_SignUp_Screens/login_mode.dart';
 import 'package:betapp/View/onboarding/onbording.dart';
 import 'package:betapp/utills/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,7 +68,7 @@ class _SplashScreenState extends State<SplashScreen> {
       if (user == null) {
         Future.delayed(const Duration(seconds: 2)).then((value) =>
             Navigator.push(
-                context, MaterialPageRoute(builder: (_) => Onbording())));
+                context, MaterialPageRoute(builder: (_) => LoginMode())));
       } else {
         Future.delayed(const Duration(seconds: 2)).then((value) =>
             Navigator.pushAndRemoveUntil(
